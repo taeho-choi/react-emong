3d45148c0a702c5635f566f07bc1f52d7cd5a092
Taeho Choi <60216512+taeho-choi@users.noreply.github.com>
2022-04-08 19:36:04 +0900

    Upload build folder

--- src/pages/Board/AngryBoard.js
+++
@@ -1,31 +0,0 @@
-import React, { useEffect } from "react";
-import SideMenu from "../SideMenu.js";
-import PostList from "../PostList.js";
-
-const AngryBoard = ({ activeMenu, setActiveMenu, postList, onEdit }) => {
-  useEffect(() => {
-    setActiveMenu("angry");
-    console.log("앵그리 액티브");
-  }, []);
-
-  return (
-    <>
-      <SideMenu activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
-      <div className="content">
-        <div className="AngryBoard">
-          <div className="title">
-            🤬 분노<a>분노가 포함된 감정들입니다.</a>
-          </div>
-          <p className="line"></p>
-          <PostList
-            postList={postList}
-            onEdit={onEdit}
-            classifyFlag={"angry"}
-          />
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default AngryBoard;
